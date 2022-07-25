e443db55563318a611b06f13d6a14e0657614060
Alfio Martini <alfio.martini@gmail.com>
2022-07-25 19:25:30 -0300

    delegate shared modules to package.json.dependencies

--- marketing/config/webpack.dev.js
+++ marketing/config/webpack.dev.js
@@ -2,6 +2,7 @@ const { merge } = require("webpack-merge");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
 const commonConfig = require("./webpack.common");
+const packageJson = require("../package.json");
 
 const devConfig = {
   mode: "development",
@@ -23,7 +24,7 @@ const devConfig = {
       exposes: {
         "./MarketingApp": "./src/bootstrap",
       },
-      shared: ["react", "react-dom"],
+      shared: packageJson.dependencies,
     }),
     new HtmlWebpackPlugin({
       template: "./public/index.html",
