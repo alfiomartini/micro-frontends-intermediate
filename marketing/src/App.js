608ab918165b32c673157926aa4f546f65331a9a
Alfio Martini <alfio.martini@gmail.com>
2022-07-25 07:46:36 -0300

    add App component and fix justifyContent property in Landind's Grid

--- marketing/src/App.js
+++ marketing/src/App.js
@@ -1,7 +1,8 @@
+import React from "react";
 import { Switch, Route, BrowserRouter } from "react-router-dom";
 import { StylesProvider } from "@material-ui/core";
-import { Landing } from "./components/Landing";
-import { Pricing } from "./components/Pricing";
+import Landing from "./components/Landing";
+import Pricing from "./components/Pricing";
 
 function App() {
   return (
--- marketing/src/bootstrap.js
+++ marketing/src/bootstrap.js
@@ -1,9 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import App from "./App";
 
 // Mount function to start the App
 function mount(el) {
-  ReactDOM.render(<h1>React in Marketing</h1>, el);
+  ReactDOM.render(<App />, el);
 }
 
 // If we are in development and in isolation, call mount immediately
