887ed6e2d2ff5689c70ad258e2405a0dba5772d4
Alfio Martini <alfio.martini@gmail.com>
2022-07-25 10:23:44 -0300

    configure webpack.dev.js to use ModuleFederationPlugin

--- marketing/config/webpack.dev.js
+++ marketing/config/webpack.dev.js
@@ -1,5 +1,6 @@
 const { merge } = require("webpack-merge");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
 const commonConfig = require("./webpack.common");
 
 const devConfig = {
@@ -16,6 +17,13 @@ const devConfig = {
   // error: failed to load source map for webpack...""
   devtool: "source-map",
   plugins: [
+    new ModuleFederationPlugin({
+      name: "marketing",
+      filename: "remoteEntry.js",
+      exposes: {
+        "./Marketing": "./src/bootstrap",
+      },
+    }),
     new HtmlWebpackPlugin({
       template: "./public/index.html",
     }),
