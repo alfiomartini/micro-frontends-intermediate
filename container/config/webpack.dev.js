61eefed997beb4632bceab71c1bb4c2d94e77147
Alfio Martini <alfio.martini@gmail.com>
2022-07-26 10:55:53 -0300

    fix II: add workflow for installation, building and aws deployment

--- container/config/webpack.dev.js
+++ container/config/webpack.dev.js
@@ -1,4 +1,5 @@
 const { merge } = require("webpack-merge");
+
 const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
 const commonConfig = require("./webpack.common");
 const packageJson = require("../package.json");
