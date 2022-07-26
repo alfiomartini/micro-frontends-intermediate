a2cd1b56397e6a43a166226e656bf65fe278793c
Alfio Martini <alfio.martini@gmail.com>
2022-07-26 00:12:23 -0300

    add webpack.prod.js to container

--- container/config/webpack.common.js
+++ container/config/webpack.common.js
@@ -1,3 +1,5 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
 module.exports = {
   module: {
     rules: [
@@ -16,4 +18,9 @@ module.exports = {
       },
     ],
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./public/index.html",
+    }),
+  ],
 };
