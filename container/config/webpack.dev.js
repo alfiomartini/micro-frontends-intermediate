703b5075eaf424f0fa1635f5f5e6053622bee006
Alfio Ricardo de Brito Martini <amartini@daitan.com>
2022-07-30 11:38:18 -0300

    change port of marketing app

--- container/config/webpack.dev.js
+++ container/config/webpack.dev.js
@@ -21,7 +21,7 @@ const devConfig = {
     new ModuleFederationPlugin({
       name: "container",
       remotes: {
-        marketing: "marketing@http://localhost:8081/remoteEntry.js",
+        marketing: "marketing@http://localhost:8082/remoteEntry.js",
       },
       shared: packageJson.dependencies,
     }),
